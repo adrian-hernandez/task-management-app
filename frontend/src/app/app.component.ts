9f7ed91812df17654c0476a4ef2dce84b3c1748f
adrian-hernandez <1hernandezadrian1@gmail.com>
2024-11-21 13:22:34 -0800

    Fix issue with FE connecting to BE due to missing HTTP connector

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from './services/task.service';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +12,40 @@ import { FormsModule } from '@angular/forms';
   imports: [RouterOutlet, CommonModule, FormsModule],
   standalone: true
 })
-export class AppComponent {
-  newTask = {
+export class AppComponent implements OnInit {
+  newTask: Task = {
     title: '',
     description: '',
     completed: false
   };
   
-  tasks: any[] = [];
+  tasks: Task[] = [];
 
-  addTask() {
-    this.tasks.push({...this.newTask});
-    this.newTask.title = '';
-    this.newTask.description = '';
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
   }
 
-  toggleComplete(task: any) {
+  loadTasks() {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
+
+  addTask() {
+    this.taskService.createTask(this.newTask).subscribe(task => {
+      this.tasks.push(task);
+      this.newTask = {
+        title: '',
+        description: '',
+        completed: false
+      };
+    });
+  }
+
+  toggleComplete(task: Task) {
     task.completed = !task.completed;
+    this.taskService.updateTask(task).subscribe();
   }
 }
