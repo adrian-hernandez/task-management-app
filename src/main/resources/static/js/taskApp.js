ee3f1a2c8616c5a513e99a13200d4c96fbd7c6ac
adrian-hernandez <1hernandezadrian1@gmail.com>
2024-11-20 17:02:30 -0800

    Update Typescript to adhere with Angular standards

--- src/main/resources/static/js/taskApp.js
+++ src/main/resources/static/js/taskApp.js
@@ -1,38 +1,30 @@
-// taskApp.js
-
-const app = angular.module("taskApp", []);
-
-app.controller("TaskController", function($scope, $http) {
-    $scope.tasks = [];
-    $scope.newTask = {};
-
-    // Load tasks from the backend
-    $scope.loadTasks = function() {
-        $http.get("/tasks").then(response => {
-            $scope.tasks = response.data;
-        });
-    };
-
-    // Add new task
-    $scope.addTask = function() {
-        $http.post("/tasks", $scope.newTask).then(response => {
-            $scope.tasks.push(response.data);
-            $scope.newTask = {};
-        });
-    };
-
-    // Delete task
-    $scope.deleteTask = function(task) {
-        // Here we are directly removing the task from the UI
-        const index = $scope.tasks.indexOf(task);
-        if (index > -1) {
-            $scope.tasks.splice(index, 1); // Remove task from the array
-        }
-
-        // You would want to add a call to delete the task from the backend as well:
-        // $http.delete('/tasks/' + task.id);
-    };
-
-    // Initialize by loading tasks on page load
-    $scope.loadTasks();
-});
+var app = angular.module("taskApp", []);
+app.controller("TaskController", ["$scope", "$http", function ($scope, $http) {
+        $scope.tasks = [];
+        $scope.newTask = {};
+        // Load tasks from the backend
+        $scope.loadTasks = function () {
+            $http.get("/tasks").then(function (response) {
+                $scope.tasks = response.data;
+            });
+        };
+        // Add new task
+        $scope.addTask = function () {
+            $http.post("/tasks", $scope.newTask).then(function (response) {
+                $scope.tasks.push(response.data);
+                $scope.newTask = {};
+            });
+        };
+        // Delete task
+        $scope.deleteTask = function (task) {
+            var index = $scope.tasks.indexOf(task);
+            if (index > -1) {
+                $scope.tasks.splice(index, 1);
+            }
+            // You would want to add a call to delete the task from the backend as well:
+            // $http.delete('/tasks/' + task.id);
+        };
+        // Initialize by loading tasks on page load
+        $scope.loadTasks();
+    }]);
+//# sourceMappingURL=taskApp.js.map
